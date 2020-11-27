befec69d5cace1b164a0f7a45fda73d28c532c91
sakibalamin <sakibalamin162@gmail.com>
2020-11-27 14:04:03 +0600

    added dotenv to store db cred.

--- db/database.go
+++ db/database.go
@@ -3,14 +3,27 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 // CreateDatabase ...
 func CreateDatabase() (*sql.DB, error) {
-	db, err := sql.Open("mysql", "sakibalamin:1620801042@/book_db")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbUsername := os.Getenv("dbUsername")
+	dbPassword := os.Getenv("dbPassword")
+
+	db, err := sql.Open("mysql", dbUsername+":"+dbPassword+"@/book_db")
 	if err != nil {
 		return nil, err
 	}
+
 	err = db.Ping()
 	if err != nil {
 		return nil, err
--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"restapisql/app"
 	"restapisql/db"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -19,11 +21,18 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	PORT := os.Getenv("PORT")
+
 	app := &app.App{
 		Router:   mux.NewRouter(),
 		Database: database,
 	}
 
 	app.HandleRequests()
-	log.Fatal(http.ListenAndServe(":8080", app.Router))
+	log.Fatal(http.ListenAndServe(":"+PORT, app.Router))
 }

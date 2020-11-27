1b8656d50fcd39dd8a197958c2e56592e1c3454e
sakibalamin <sakibalamin162@gmail.com>
2020-11-27 12:20:29 +0600

    getBook added

--- app/app.go
+++ app/app.go
@@ -41,7 +41,52 @@ func (app *App) getBooks(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(books)
 }
 
+func (app *App) getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	// suppose we could have more than one book with this ID
+	rows, err := app.Database.Query("select * from Book where id = ?", params["id"])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	var books []Book
+	b := &Book{}
+	for rows.Next() {
+		err := rows.Scan(&b.Name, &b.ID)
+		if err != nil {
+			log.Fatal(err)
+		}
+		books = append(books, Book{Name: b.Name, ID: b.ID})
+	}
+
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	json.NewEncoder(w).Encode(books)
+}
+
+func (app *App) createBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *App) updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *App) deleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
 // HandleRequests ...
 func (app *App) HandleRequests() {
 	app.Router.HandleFunc("/api/books", app.getBooks).Methods("GET")
+	app.Router.HandleFunc("/api/books/{id}", app.getBook).Methods("GET")
+	app.Router.HandleFunc("/api/books/", app.createBook).Methods("POST")
+	app.Router.HandleFunc("/api/books/{id}", app.updateBook).Methods("PUT")
+	app.Router.HandleFunc("/api/books/{id}", app.deleteBook).Methods("DELETE")
 }
